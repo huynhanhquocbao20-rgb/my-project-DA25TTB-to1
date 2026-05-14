92144082809dc3e7856ee0e981342fe98b7ad256
haha <baodeptrai@example.com>
2026-05-14 09:10:56 +0700

    du an

---
+++ js/lib.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="vi">
+    <head><title>thông tin cá nhân</title>
+          <meta charset="utf-8">
+          </head>
+        <body>
+            <h3>giới thiệu bản thân</h3>
+              <ul>
+                <li>
+                    <a></a>
+                </li>
+              </ul>
+        </body>
+</html>
