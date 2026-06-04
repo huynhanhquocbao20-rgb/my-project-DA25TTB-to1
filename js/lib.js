888c3fa103992f4ed3cd5c388b7b8cba5013700f
bao <huynhanhquocbao20@gmail.com>
2026-06-04 10:31:50 +0700

    du an 4

--- js/lib.js
+++ js/lib.js
@@ -1,9 +1,14 @@
-<!DOCTYPE html>
-<html lang="vi">
-    <head><title>thông tin cá nhân</title>
-          <meta charset="utf-8">
-          </head>
-        <body>
-                    <a></a>
-        </body>
-</html>
+/*Viết mã Javascript*/
+function printBCC(n)
+{
+    let result = "";
+
+    let i = 1;
+    while(i<=10)
+    {
+        result += `${n} x ${i} = ${n*i} <br>`; 
+        i++;
+    }
+    //Đưa kết quả vào thẻ div có id là "result"
+    document.getElementById("result").innerHTML = result;
+}
