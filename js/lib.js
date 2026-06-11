924df78e86c3075635a2892dbec83ba545f85050
hoatulip <huynhanhquocbao20@gmail.com>
2026-06-11 10:19:05 +0700

    hoatuplip

--- js/lib.js
+++ js/lib.js
@@ -1,4 +1,11 @@
 /*Viết mã Javascript*/
+const products={
+    name: "Hoa cúc Indo",
+    price: 2500000,
+    description: "Từ lâu, hoa cúc đã được trồng để làm cảnh, trang trí trong những sự kiện, ngày lễ trọng đại. Theo quan niệm của người xưa, hoa cúc tượng trưng cho sự trường thọ, thể hiện sự hiếu thảo, lòng biết ơn của con cháu đối với cha mẹ, ông bà, tổ tiên.",
+    link: "chi-tiet.html",
+    image: "../assets/images/cuc-indo.jpg"
+}
 function printBCC(n)
 {
     let result = "";
